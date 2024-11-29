a4e0b29461a25c8c2596d6c69a47880b3344826c
A00832425 <A00832425@tec.mx>
2024-11-28 22:26:01 -0600

    comit 9

--- src/func/func.h
+++ src/func/func.h
@@ -14,10 +14,3 @@ using namespace std;
 
 unsigned int factorial(unsigned int number);
 
-vector<int> computeLPS(const string& pattern)
-
-int containsCode(const string& transmissionFile, const string& mcodeFile) 
-
-tuple<int, int, string> findLongestPalindrome(const string& transmissionFile) 
-
-tuple<int, int, string> findLongestCommonSubstring(const string& transmission1File, const string& transmission2File)
